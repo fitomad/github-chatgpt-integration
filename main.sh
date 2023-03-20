31565a5ede6dd3350079e973ac32778019f017f0
Adolfo <adolfo.vera@outlook.com>
2023-03-20 16:48:41 +0100

    Added the --dev-lang parameter

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-python /main.py --openai-api-key "$1" --github-token "$2" --github-pr-id "$3" --openai-engine "$4" --openai-temperature "$5" --openai-max-tokens "$6" --mode "$7"
+python /main.py --openai-api-key "$1" --github-token "$2" --github-pr-id "$3" --dev-lang "$4" --openai-engine "$5" --openai-temperature "$6" --openai-max-tokens "$7"
